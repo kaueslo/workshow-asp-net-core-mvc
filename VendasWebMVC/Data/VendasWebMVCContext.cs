44735224b9537b3a0f3d900be566bf873c07dc43
Kaue Sloboda <kaue.sloboda@hotmail.com>
2020-01-21 10:50:25 -0300

    Criando o departamento com Scaffolding

---
+++ VendasWebMVC/Data/VendasWebMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VendasWebMVC.Models
+{
+    public class VendasWebMVCContext : DbContext
+    {
+        public VendasWebMVCContext (DbContextOptions<VendasWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VendasWebMVC.Models.Departamento> Departamento { get; set; }
+    }
+}
