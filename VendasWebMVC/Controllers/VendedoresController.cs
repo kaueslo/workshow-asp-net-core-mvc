2df3b30a2c09a0bef262a2a868531e5654a19d6c
Kaue Sloboda <kaue.sloboda@hotmail.com>
2020-01-23 08:33:21 -0300

    Fazendo os metodos e a view para inserir Vendedores

--- VendasWebMVC/Controllers/VendedoresController.cs
+++ VendasWebMVC/Controllers/VendedoresController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VendasWebMVC.Models;
 using VendasWebMVC.Services;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -25,6 +26,19 @@ namespace VendasWebMVC.Controllers
 
 			var lista = _vendedorServico.AcharTodos();
 			return View(lista);
+		}
+
+		public IActionResult Criar()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Criar(Vendedor vendedor)
+		{
+			_vendedorServico.Inserir(vendedor);
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
--- VendasWebMVC/Services/ServicoVendedor.cs
+++ VendasWebMVC/Services/ServicoVendedor.cs
@@ -20,5 +20,11 @@ namespace VendasWebMVC.Services
 			return _context.Vendedor.ToList();
 		}
 
+		public void Inserir(Vendedor obj)
+		{
+			_context.Add(obj);
+			_context.SaveChanges();
+		}
+
 	}
 }
