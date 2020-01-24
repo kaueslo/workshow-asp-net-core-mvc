71cb6b5f77193dbfeb817068e132228eac35a642
Kaue Sloboda <kaue.sloboda@hotmail.com>
2020-01-24 08:57:09 -0300

    Mudando a formatacao de data e valores numericos

--- VendasWebMVC/Models/Vendedor.cs
+++ VendasWebMVC/Models/Vendedor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace VendasWebMVC.Models
 {
@@ -10,8 +11,16 @@ namespace VendasWebMVC.Models
 	{
 		public int Id { get; set; }
 		public string Nome { get; set; }
+
+		[DataType(DataType.EmailAddress)]
 		public string Email { get; set; }
+
+		[Display(Name = "Data de Aniversário")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
 		public DateTime dtAniversario { get; set; }
+
+		[DisplayFormat(DataFormatString ="{0:F2}")]
 		public double Salario { get; set; }
 		public Departamento Departamento { get; set; }
 		public int DepartamentoId { get; set; }
