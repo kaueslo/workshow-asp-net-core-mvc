c65e37ec95c805468caa5828cfcdfdb685a17730
Kaue Sloboda <kaue.sloboda@hotmail.com>
2020-01-27 08:41:09 -0300

    Iniciando o mvc do registro de vendas

---
+++ VendasWebMVC/Controllers/RegistroVendedorsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWebMVC.Controllers
+{
+    public class RegistroVendedorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult BuscaSimples()
+		{
+			return View();
+		}
+
+		public IActionResult BuscaAgrupada()
+		{
+			return View();
+		}
+	}
+}
